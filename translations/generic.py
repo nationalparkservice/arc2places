4022b754b46a44dd7bffb76d352951558005f391
Regan Sarwas <regan_sarwas@nps.gov>
2015-05-22 15:58:32 -0800

    rewrite translation code
    made it more maintainable and generic

--- translations/generic.py
+++ translations/generic.py
@@ -1,41 +1,97 @@
+def valueof(name, altnames, attributes, no_values=('null', 'none', 'unknown')):
+    value = None
+    if name in attributes:
+        value = attributes[name]
+    else:
+        if name in altnames:
+            for altname in altnames[name]:
+                if altname in attributes:
+                    value = attributes[altname]
+    if value:
+        value = value.strip()
+        if value.lower() in no_values:
+            value = None
+    return value
+
+
+def simplemap(gisnames, altnames, attributes):
+    tags = {}
+    for gisname in gisnames:
+        value = valueof(gisname, altnames, attributes)
+        if value:
+            tags[attributes[gisname]] = value
+    return tags
+
+
+def fancymap(name, altnames, attrs, tags, valuemaps):
+    value = valueof(name, altnames, attrs)
+    if value and value in valuemaps:
+        values = valuemaps[value]
+        for i in range(len(values)):
+            if values[i]:
+                tags[tags[i]] = values[i]
+
+
+# noinspection PyPep8Naming
 def filterTags(attrs):
     if not attrs:
         return
-    tags = {}
+
+    # namemap provides a mapping from the GIS field name to the places tag
+    # when there is a one to one mapping of values
+    # This is based on the Places Data Schema revision 6 (5/18/2015)
+    # https://github.com/nationalparkservice/places-data/wiki/Places-Data-Schema-Guide
+    # with ogr2osm, the column names are case sensitive
+    # with arc2osm, all column names are converted to upper case
+    # all osm/places tags should be lower case
 
     namemap = {
-        # 'EGIS COLUMN NAME' : 'osm/places tag name'
-        # with ogr2osm, the column names are case sensitive
-        # with arc2osm, all column names are converted to upper case
-        # all osm/places tags should be lower case
-        # 'PLACESID'   :Ignore
+        # 'EGIS_COLUMN_NAME' : 'osm/places tag name'
+        # 'PLACESID': 'nps:places_id'  # Ignore - managed by system
         'UNITCODE': 'nps:unit_code',
-        'UNITNAME': 'nps:unitname',
-        'GROUPCODE': 'nps:groupcode',
-        'REGIONCODE': 'nps:regioncode',
-        'LOCATIONID': 'nps:locationid',
-        'ASSETID': 'nps:assetid',
-        'ISEXTANT': 'nps:isextant',
-        'MAPMETHOD': 'nps:mapmethod',
+        'UNITNAME': 'nps:unit_name',
+        'GROUPCODE': 'nps:group_code',
+        'REGIONCODE': 'nps:region_code',
+        'LOCATIONID': 'nps:location_id',
+        'ASSETID': 'nps:asset_id',
+        'ISEXTANT': 'nps:is_extant',
+        'MAPMETHOD': 'nps:map_method',
         'MAPSOURCE': 'source',
-        'SOURCESCALE': 'nps:sourcescale',
-        'SOURCEDATE': 'nps:sourcedate',
-        'XYERROR': 'nps:xyerror',
+        'SOURCESCALE': 'nps:source_scale',
+        'SOURCEDATE': 'nps:source_date',
+        'XYERROR': 'nps:xy_error',
         'NOTES': 'note',
         'RESTRICTION': 'nps:restriction',
         'DISTRIBUTE': 'nps:distribute',
-        'CREATEDATE': 'nps:createdate',
-        'CREATEUSER': 'nps:createuser',
-        'EDITDATE': 'nps:editdate',
-        'EDITUSER': 'nps:edituser',
-        'FEATUREID': 'nps:featureid',
-        'GEOMETRYID': 'nps:source_id'
+        'CREATEDATE': 'nps:create_date',
+        'CREATEUSER': 'nps:create_user',
+        'EDITDATE': 'nps:edit_date',
+        'EDITUSER': 'nps:edit_user',
+        'FEATUREID': 'nps:feature_id',
+        'GEOMETRYID': 'nps:source_id',
+        # helpful aliases
+        'NAME': 'name'
     }
 
-    for gisname in namemap:
-        if gisname in attrs:
-            value = attrs[gisname]
-            if value and value != 'Unknown':
-                tags[namemap[gisname]] = value
+    alt_names = {
+        'UNITCODE': ['UNIT_CODE'],
+        'UNITNAME': ['UNIT_NAME'],
+        'GROUPCODE': ['GROUP_CODE'],
+        'REGIONCODE': ['REGION_CODE'],
+        'LOCATIONID': ['LOCATION_ID', 'FMSSID', 'FMSS_ID'],
+        'ASSETID': ['ASSET_ID'],
+        'ISEXTANT': ['IS_EXTANT'],
+        'MAPMETHOD': ['MAP_METHOD'],
+        'MAPSOURCE': ['MAP_SOURCE'],
+        'SOURCESCALE': ['SOURCE_SCALE'],
+        'SOURCEDATE': ['SOURCE_DATE'],
+        'XYERROR': ['XY_ERROR'],
+        'CREATEDATE': ['CREATE_DATE', 'CREATED_DATE'],
+        'CREATEUSER': ['CREATE_USER', 'CREATED_USER'],
+        'EDITDATE': ['EDIT_DATE', 'LAST_EDIT_DATE', 'LAST_EDITED_DATE'],
+        'EDITUSER': ['EDIT_USER', 'LAST_EDIT_USER', 'LAST_EDITED_USER'],
+        'FEATUREID': ['FEATURE_ID'],
+        'GEOMETRYID': ['GEOMETRY_ID'],
+    }
 
-    return tags
+    return simplemap(namemap, alt_names, attrs)
--- translations/roads.py
+++ translations/roads.py
@@ -1,112 +1,103 @@
 import generic
 
 
+# noinspection PyPep8Naming
 def filterTags(attrs):
     if not attrs:
         return
-    tags = generic.filterTags(attrs)
 
-    # defaults
-    tags['highway'] = 'road'
-    tags['oneway'] = 'no'
-
-    # FIXME renderer uses name for labeling, should we put label in name?
     namemap = {
         'RDNAME': 'name',
-        'RDALTNAMES': 'nps:rdaltnames',
-        'RDLABEL': 'nps:rdlabel',
-        'RDSTATUS': 'nps:rdstatus',
-        'RDCLASS': 'nps:rdclass',
+        'RDALTNAMES': 'nps:road_alt_names',
+        'RDLABEL': 'nps:road_label',
+        'RDSTATUS': 'nps:road_status',
+        'RDCLASS': 'nps:road_class',
+        'RDMAINTAINER': 'nps:road_maintainer',
         'LANES': 'lanes',
-        'ROUTEID': 'nps:routeid',
-        'RTENUMBER': 'nps:rtenumber'}
+        'ROUTEID': 'nps:route_id',
+        'RTENUMBER': 'nps:route_number'
+    }
 
-    status = {
-        'Existing': ('yes', None),  # access,highway
-        'Decommissioned': ('no', None),
-        'Temporarily Closed': ('no', None),
-        'Proposed': ('no', 'proposed'),  # conflict with class
-        'Planned': ('no', 'proposed'),  # conflict with class
-        'Unknown': ('no', None)}
+    altnames = {
+        'RDNAME': ['NAME', 'ROADNAME', 'RD_NAME', 'ROAD_NAME'],
+        'RDALTNAMES': ['ALTNAMES', 'ROADALTNAMES', 'RD_ALTNAMES',
+                       'ROAD_ALTNAMES'],
+        'RDLABEL': ['LABEL', 'ROADLABELE', 'RD_LABEL', 'ROAD_LABEL'],
+        'RDSTATUS': ['STATUS', 'ROADSTATUS', 'RD_STATUS', 'ROAD_STATUS'],
+        'RDCLASS': ['CLASS', 'ROADCLASS', 'RD_CLASS', 'ROAD_CLASS'],
+        'RDMAINTAINER': ['MAINTAINER', 'ROADMAINTAINER', 'RD_MAINTAINER',
+                         'ROAD_MAINTAINER'],
+        'ROUTEID': ['ROUTE', 'ROUTE_ID'],
+        'RTENUMBER': ['RTE_NUMBER', 'ROUTENUMBER', 'ROUTE_NUMBER']
+    }
 
-    # FIXME: which governs highway tag from class or status?
-    # I think it should be status, so it goes second
+    # must do class before status because of overlap with proposed status
+    multimaps = {
+        'RDCLASS': (
+            ['highway', 'access', '4wd_only'],
+            {
+                'Primary': ('primary', None, None),
+                'Secondary': ('secondary', None, None),
+                'Local': ('residential', None, None),
+                '4WD': ('road', None, 'yes'),
+                'Service': ('service', 'private', None),
+                'Private': ('road', None, None)
+            }
+        ),
+        'RDSTATUS': (
+            ['access', 'highway'],
+            {
+                'Existing': ('yes', None),
+                'Decommissioned': ('no', None),
+                'Temporarily Closed': ('no', None),
+                'Proposed': ('no', 'proposed'),  # overlap with class
+                'Planned': ('no', 'proposed')  # overlap with class
+            }
+        ),
+        'RDSURFACE': (
+            ['surface'],
+            {
+                'Asphalt': 'asphalt',
+                'Concrete': 'concrete',
+                'Brick/Pavers': 'paving_stones',
+                'Cobblestone': 'cobblestone',
+                'Gravel': 'gravel',
+                'Sand': 'sand',
+                'Native or Dirt': 'ground',
+                'Unpaved Other': 'unpaved',
+                'Paved Other': 'paved'
+            }
+        ),
+        'ONEWAY': (
+            ['oneway'],
+            {
+                'With Digitized': 'yes',
+                'Against Digitized': '-1'
+            }
+        )
+    }
 
-    rclass = {
-        'Unknown': 'road',  # highway
-        'Primary': 'primary',
-        # 'Primary'  :'primary_link',
-        'Secondary': 'secondary',
-        # 'Secondary':'secondary_link',
-        # 'Local'    :'tertiary',
-        # 'Local'    :'tertiary_link',
-        'Local': 'residential',
-        # 'Local'    :'trunk',
-        # 'Local'    :'trunk_link',
-        # 'Local'    :'unclassified',
-        '4WD': 'road',  # 4wd_only=yes
-        'Service': 'service',
-        'Private': 'road'}  # access=private
+    # generics tags
+    tags = generic.filterTags(attrs)
 
-    surface = {
-        'Asphalt': 'asphalt',
-        'Concrete': 'concrete',
-        'Brick/Pavers': 'paving_stones',
-        'Cobblestone': 'cobblestone',
-        'Gravel': 'gravel',
-        'Sand': 'sand',
-        'Native or Dirt': 'ground',
-        'Unpaved Other': 'unpaved',
-        'Paved Other': 'paved',
-        'Unknown': None}
+    # default tags
+    tags['highway'] = 'road'
+    tags['oneway'] = 'no'
+    tags['access'] = 'no'
 
-    oneway = {
-        'With Digitized': 'yes',
-        'Against Digitized': '-1'}
-    # 'NULL' :'no'}
+    # simple tags
+    tags.update(generic.simplemap(namemap, altnames, attrs))
 
-    for gisname in namemap:
-        if gisname in attrs:
-            value = attrs[gisname].strip()
-            if value:
-                tags[namemap[gisname]] = value
+    # multi tags
+    for name in multimaps:
+        tagz, valuesmap = multimaps[name]
+        generic.fancymap(name, altnames, attrs, tagz, valuesmap)
 
-    if 'RDCLASS' in attrs and attrs['RDCLASS']:
-        class_value = attrs['RDCLASS'].strip()
-        for gisclass in rclass:
-            if gisclass == class_value:
-                osmclass = rclass[gisclass]
-                if osmclass:
-                    tags['highway'] = osmclass
-        if class_value == '4WD':
-            tags['4wd_only'] = 'yes'
-        if class_value == 'Private':
-            tags['access'] = 'private'
-
-    if 'RDSURFACE' in attrs and attrs['RDSURFACE']:
-        surface_value = attrs['RDSURFACE'].strip()
-        for gissurface in surface:
-            if gissurface == surface_value:
-                osmsurface = surface[gissurface]
-                if osmsurface:
-                    tags['surface'] = osmsurface
-
-    if 'ONEWAY' in attrs and attrs['ONEWAY']:
-        oneway_value = attrs['ONEWAY'].strip()
-        for gisoneway in oneway:
-            if gisoneway == oneway_value:
-                osmoneway = oneway[gisoneway]
-                if osmoneway:
-                    tags['oneway'] = osmoneway
-
-    if 'RDSTATUS' in attrs and attrs['RDSTATUS']:
-        status_value = attrs['RDSTATUS'].strip()
-        for gisstatus in status:
-            if gisstatus == status_value:
-                osmstatus = status[gisstatus]
-                if osmstatus[0]:
-                    tags['access'] = osmstatus[0]
-                if osmstatus[1]:
-                    tags['highway'] = osmstatus[1]
+    # special case when status is proposed/planned, then put class in proposed
+    if tags['highway'] == 'proposed':
+        name = 'RDCLASS'
+        tagz, valuesmap = multimaps[name]
+        tagz = ['proposed']
+        generic.fancymap(name, altnames, attrs, tagz, valuesmap)
 
     return tags
