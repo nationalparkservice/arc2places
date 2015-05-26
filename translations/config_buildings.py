1513d0dc6ca839b58e334d19882bc26fd40d3541
Regan Sarwas <regan_sarwas@nps.gov>
2015-05-26 11:48:49 -0800

    Added Buildings

---
+++ translations/config_buildings.py
@@ -0,0 +1,31 @@
+# Based on the Places Data Schema revision 6 (5/18/2015)
+# https://github.com/nationalparkservice/places-data/wiki/Places-Data-Schema-Guide
+# with ogr2osm, the GIS field names are case sensitive
+# with arc2osm, all GIS field names are converted to upper case
+# all osm/Places tags should be lower case
+
+# All config files must have defaults, fieldmap, altnames and valuemap
+
+# default tags for generic features in Places
+defaults = {
+    'building': 'yes'
+}
+
+# values map one to one from GIS field name to Places tag
+fieldmap = {
+    # GIS_FieldName : Places Tag
+    'COMMON_NAME': 'name',
+    'BUILDING_FCODE': 'nps:building_fcode',
+    'ADMIN_TYPE': 'nps:admin_type',
+    'FEDERAL_ENTITY_TYPE': 'nps:federal_entity_type',
+    'POLYGON_NOTES': 'nps:polygon_notes'
+}
+
+# alternate GIS field names.
+altnames = {
+    # GIS Standard FieldName: List of alternate spellings of field name
+}
+
+valuemap = {
+    # GIS_FieldName : {GIS_Value: {tag:value, ... }, ...}
+}
