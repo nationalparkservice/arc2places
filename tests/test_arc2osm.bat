8fc8cc2bc147f13c45d022a6c649c5f610847834
Regan Sarwas <regan_sarwas@nps.gov>
2015-06-01 07:55:33 -0800

    Test fgdb and output

---
+++ tests/test_arc2osm.bat
@@ -0,0 +1,5 @@
+C:\Python27\ArcGIS10.3\python.exe ..\arc2osm.py test.gdb\PARKINGLOTS_PY -o test_parking.osm -f -t parkinglots -c
+C:\Python27\ArcGIS10.3\python.exe ..\arc2osm.py test.gdb\ROADS_LN -o test_parking.osm -f -t roads -c
+C:\Python27\ArcGIS10.3\python.exe ..\arc2osm.py test.gdb\TRAILS_LN -o test_parking.osm -f -t trails -c
+C:\Python27\ArcGIS10.3\python.exe ..\arc2osm.py test.gdb\POI_PT -o test_parking.osm -f -t poi -c
+C:\Python27\ArcGIS10.3\python.exe ..\arc2osm.py test.gdb\multipoint -o test_generic_mp.osm -f -t generic -c
