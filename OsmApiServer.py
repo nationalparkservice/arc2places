c14d25b6a1a32947f158ac131ccb6ad0ef87fdc8
Regan Sarwas <rsarwas@gmail.com>
2015-07-25 20:45:53 -0800

    Clean up OsmApiServer class

--- OsmApiServer.py
+++ OsmApiServer.py
@@ -27,23 +27,26 @@ from secrets import *
 
 class OsmApiServer:
     def __init__(self, name='', baseurl=''):
+        self.name = name
         self.max_waynodes = None
         self.max_elements = None
+        self.version = '0.6'
         self.server_available = False
         self.called_capabilities = False
         self._verbose = False
         self.username = os.getenv('USERNAME')
+        self.oauth = None
+        self.error = None
         
         # TODO: Sort this out, use name or baseurl to pick server style
         self.baseurl = baseurl
-        self.error, self.baseurl, self.tokens = self.setup(name)
 
     def turn_verbose_on(self):
         self._verbose = True
         
     def turn_verbose_off(self):
         self._verbose = True
-        
+
     def get_capabilities(self):
         def get_capabilities_from_server():
             self.called_capabilities = True
@@ -56,16 +59,13 @@ class OsmApiServer:
 
         return self.max_waynodes, self.max_elements
 
-    # TODO Add other properties
-    # TODO Add functions from osm2places
-
-    def setup(self, site, options=None):
-        url = secrets[site]['url']
-        client_token = secrets[site]['consumer_key']
-        client_secret = secrets[site]['consumer_secret']
+    def connect(self):
+        self.baseurl = secrets[self.name]['url']
+        client_token = secrets[self.name]['consumer_key']
+        client_secret = secrets[self.name]['consumer_secret']
 
         # Get Request Tokens
-        request_url = url + '/oauth/request_token'
+        request_url = self.baseurl + '/oauth/request_token'
         if self._verbose:
             print "Getting Request Tokens from", request_url
             print "Client Token", client_token
@@ -82,10 +82,14 @@ class OsmApiServer:
         # Authorize User
         if self._verbose:
             print "Authorizing user"
-        auth_url = url + '/oauth/add_active_directory_user'
-        error, userid, username = self.getuseridentity(site, request_tokens, options)
+        auth_url = self.baseurl + '/oauth/add_active_directory_user'
+        error, userid, username = self.getuseridentity(request_tokens)
         if error:
-            return error, userid, username
+            # FIXME: clean up error message
+            self.error = error + userid + username
+            self.oauth = None
+            return
+
         auth_data = {
             'query': request_tokens,
             'userId': userid,
@@ -98,12 +102,16 @@ class OsmApiServer:
         # Ignore the response (display name)
         res = requests.post(auth_url, data=json.dumps(auth_data), headers=header)
         if res.status_code != 200:
-            return 'Authorization Error', res.status_code, res.text
+            # FIXME: clean up error message
+            self.error =  'Authorization Error' + str(res.status_code) + res.text
+            self.oauth = None
+            return
+
         if self._verbose:
             print "Authorized user", res.text
 
         # Get Access Tokens
-        access_url = url + '/oauth/access_token'
+        access_url = self.baseurl + '/oauth/access_token'
         if self._verbose:
             print "Getting Access Tokens from", access_url
         auth = ('OAuth ' +
@@ -112,7 +120,9 @@ class OsmApiServer:
         header = {'authorization': auth}
         res = requests.request('post', url=access_url, headers=header)
         if res.status_code != 200:
-            return 'Access Error', res.status_code, res.text
+            self.error = 'Access Error' + str(res.status_code) + res.text
+            self.oauth = None
+            return
 
         access_tokens = self.simplifydict(urlparse.parse_qs(res.text))
         if self._verbose:
@@ -120,15 +130,13 @@ class OsmApiServer:
             print "Access Secret", access_tokens['oauth_token_secret']
 
         # Initialize the Oauth object to create signed requests
-        oauth = OAuth1Session(client_token,
-                              client_secret=client_secret,
-                              resource_owner_key=access_tokens['oauth_token'],
-                              resource_owner_secret=access_tokens[
-                                  'oauth_token_secret'])
-        return None, url, oauth
+        self.oauth = OAuth1Session(client_token,
+                                   client_secret=client_secret,
+                                   resource_owner_key=access_tokens['oauth_token'],
+                                   resource_owner_secret=access_tokens['oauth_token_secret'])
+        return
 
-    # noinspection PyUnusedLocal
-    def getuseridentity(self, site, request_tokens, options=None):
+    def getuseridentity(self, request_tokens):
         if not self.username:
             return 'User Error', 0, 'No user given'
 
@@ -165,29 +173,39 @@ class OsmApiServer:
                 newdict[key] = item
         return newdict
 
-    def openchangeset(self, oauth, root):
+    def createchangeset(self, author, comment=None):
         # return Error (str), Changeset Id (str)
+        if not self.oauth:
+            self.connect()
+        if not self.oauth:
+            return None
+        if not author:
+            self.error = 'No Changeset author provided'
+            return None
         if self._verbose:
             print 'Create change set'
+        url = self.baseurl + '/api/' + self.version + '/changeset/create'
         osm_changeset_payload = ('<osm><changeset>'
-                                 '<tag k="created_by" v="arc2places"/>'
-                                 '<tag k="comment" v="upload of OsmChange file"/>'
-                                 '</changeset></osm>')
+                                 '<tag k="created_by" v="{0}"/>').format(author)
+        if comment:
+            osm_changeset_payload += '<tag k="comment" v="upload of OsmChange file"/>'
+        osm_changeset_payload += '</changeset></osm>'
         try:
-            resp = oauth.put(root + '/api/0.6/changeset/create',
+            resp = self.oauth.put(url,
                              data=osm_changeset_payload,
                              headers={'Content-Type': 'text/xml'})
         except requests.exceptions.ConnectionError:
-            return "Unable to Connect to " + root, None
+            self.error =  "Unable to Connect to " + self.baseurl
+            return None
         if resp.status_code != 200:
             baseerror = "Failed to open changeset. Status: {0}, Response: {0}"
-            error = baseerror.format(resp.status_code, resp.text)
-            return error, None
+            self.error = baseerror.format(resp.status_code, resp.text)
+            return None
         else:
             cid = resp.text
         if self._verbose:
             print "Created change set", cid
-        return None, cid
+        return cid
 
     def uploadchangeset(self, oauth, root, cid, change, options=None):
         # return Error (str), Upload Response (str)
