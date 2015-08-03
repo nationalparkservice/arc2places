3512ab00e616c020f89aeac3c87fc3a9612057ee
Regan Sarwas <regan_sarwas@nps.gov>
2015-08-03 11:43:48 -0800

    Add debug options to Logger

--- Logger.py
+++ Logger.py
@@ -1,9 +1,14 @@
 from __future__ import print_function
-import datetime
 
 
 class Logger:
+    def __init__(self):
+        self._debug = False
+
     def debug(self, text):
+        if self._debug:
+            return
+        import datetime
         print('DEBUG', datetime.datetime.now(), text)
 
     def error(self, text):
@@ -15,15 +20,24 @@ class Logger:
     def warn(self, text):
         print('WARN', text)
 
+    def start_debug(self):
+        self._debug = True
+
+    def stop_debug(self):
+        self._debug = False
+
 
 class ArcpyLogger(Logger):
     def __init__(self):
+        Logger.__init__(self)
         try:
             self.arcpy = __import__('arcpy')
         except ImportError:
             self.arcpy = None
 
     def debug(self, text):
+        if not self._debug:
+            return
         if self.arcpy:
             self.arcpy.AddMessage(text)
         else:
